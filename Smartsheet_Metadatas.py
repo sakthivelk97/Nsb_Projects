9a7ac6817202c74777f2d66d7e14843f81d2d5ca
Sakthivel <148680319+sakthivelk97@users.noreply.github.com>
2025-08-11 12:41:47 +0530

    Create Smartsheet_Metadatas.py

---
+++ Smartsheet_Metadatas.py
@@ -0,0 +1,15 @@
+'''This Code List out the Sheet Name(Client Name) with the Sheet ID'''
+import smartsheet
+
+# Replace with your API token
+TOKEN = 'VJMHRFRfjH3IaO3dUJKPp5sf0I8WqkzamUwIn'
+
+# Create Smartsheet client
+smartsheet_client = smartsheet.Smartsheet(TOKEN)
+
+# List your sheets
+response = smartsheet_client.Sheets.list_sheets()
+for sheet in response.data:
+    print(f"{sheet.name} — {sheet.id}")
+
+# THE END
